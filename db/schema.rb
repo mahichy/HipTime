58acdbbbfc849ce04e2636b86e734527554dd085
mahi <mohiddinoshim@yahoo.com>
2021-01-24 21:08:22 +0600

    Add completed at ability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_135110) do
+ActiveRecord::Schema.define(version: 2021_01_24_145950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_135110) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.datetime "completed_at"
   end
 
   create_table "users", force: :cascade do |t|
